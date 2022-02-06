8748cee1f0717b1b566236c98ee1e637dc41f914
沈赟杰 <shenyunjie@znrank.com>
2022-02-06 17:15:26 +0800

    update

--- document/.vuepress/config.js
+++ document/.vuepress/config.js
@@ -20,6 +20,19 @@ module.exports = {
       '/guider.md',
       '/setting.md',
       '/plugin.md',
-    ]
+    ],
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: 'cevio/nppm',
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: '查看源码',
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: 'document',
+    // 假如文档放在一个特定的分支下：
+    docsBranch: 'master',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面！'
   }
 }
