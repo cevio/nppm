78df35b6a393b605fd123801fe622e1797a6712a
沈赟杰 <shenyunjie@znrank.com>
2022-03-03 15:08:10 +0800

    update

--- packages/utils/src/middlewares/commander.ts
+++ packages/utils/src/middlewares/commander.ts
@@ -1,6 +1,9 @@
 import { Context, Next } from 'koa';
 import { HttpNotAcceptableException } from '@typeservice/exception';
 
+const lernaRegexp = /lerna\/\d+\.\d+\.\d+/;
+const npmRegexp = /npm\/\d+\.\d+\.\d+/;
+
 export function NpmCommanderLimit(...commanders: string[]) {
   return async (ctx: Context, next: Next) => {
     const commander = ((ctx.header['npm-command'] || ctx.header['referer']) as string) || '';
@@ -11,6 +14,17 @@ export function NpmCommanderLimit(...commanders: string[]) {
 
 export async function OnlyRunInCommanderLineInterface(ctx: Context, next: Next) {
   const useragent = ctx.header['user-agent'];
-  if (!/npm\/\d+\.\d+\.\d+/.test(useragent)) throw new HttpNotAcceptableException('Not support api');
-  await next();
+  const isLerna = lernaRegexp.test(useragent);
+  const isNpm = npmRegexp.test(useragent);
+  const hasNpmSession = !!ctx.header['npm-session'];
+  if (isLerna) {
+    if (!hasNpmSession) throw new HttpNotAcceptableException('Not support api');
+    if (ctx.request.body?._attachments && Object.keys(ctx.request.body._attachments).length) {
+      ctx.header['npm-command'] = 'publish';
+    }
+  } else if (!isNpm) {
+    throw new HttpNotAcceptableException('Not support api');
+  } else {
+    await next();
+  }
 }
